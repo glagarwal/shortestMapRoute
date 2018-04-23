3d8af6e5fa891c1dd017ee395203da98707cb35b
glagarwal <agarwalgaurav1995@gmail.com>
2018-04-23 00:16:30 -0400

    path traversal and ops logic (Dijkstra & random time change remaining)

--- README.md
+++ README.md
@@ -10,8 +10,9 @@ It consists of a loop such that at each iteration of the loop (which can be cont
 The time change information is available by calling a subroutine that randomly identifies one or more streets (u,v) on the map and randomly changes the travel time of these streets. The loop repeats until the vehicle arrives at the destination t. The program should output the shortest path at every iteration together with the relevant travel time change information.
 
 <b>A more realistic situations to consider would be:</b> While the above situation assumes that travel time changes on streets are synchronized with the time the vehicle arrives at an intersection, in reality, the algorithm should checks periodically traffic situations while on the street as well as well just at intersections. We may assume the period to be some time unit and travel time is measured with such unit. So the subroutine used to issue travel time change should also be synchronized with the time period. For example, if the travel time from interaction p to intersection q is τ(p,q) = 10 units, the program should check traffic and adjust path 10 times on the street (p,q). To deal with these situations, an iteration of the loop in your program should correspond to one time unit. <b>(This is not yet implemented)</b>
-
+<br>
 <b><u>Format of the input graph</u> (small example):</b>
+
 n=5 <br>
 s=1 <br>
 t=5 <br>
--- ShortestPath.java
+++ ShortestPath.java
@@ -1,3 +1,10 @@
+import java.util.*;
+
+import java.util.Arrays;
+// import org.apache.commons.lang3.ArrayUtils;
+
+import static java.lang.System.out;
+
 /**
  * <h1>Shortest Path on the map</h1>
  * This finds out the shortest path between 
@@ -9,26 +16,53 @@
  * @author Gaurav Agarwal
  * @since 2018-04-22
  */
-class ShortestPath{
+class ShortestPath {
     /**
      * Dijkstra's single source shortest path algorithm.
      * 
      * @author Gaurav Agarwal
      */
-    public String dijkstra(){
-        
-        return "";
+    public int[] dijkstra(int graph[][], int s, int t) {
+        // int path[] = new int[];
+        int path[] = {1, 3, 2, 4, 5};
+        return path;
     }
 
     /**
      * Changes time of an edge (path) 
-     * between any two vertices on the graph.
+     * between any two adjacent vertices on the graph.
      * 
      * @author Gaurav Agarwal
      */
-    public String timeChange(){
+    public int[] timeChange(int graph[][]) {
+        int changedPath[] = {-1, -1, -1};
+        return changedPath;
+    }
 
-        return "";
+    /**
+     * calculates the total distance of the path
+     * 
+     * @author Gaurav Agarwal
+     */
+    public int distance(int graph[][], int path[], int n){
+        int dist = 0;
+        for(int i = 0; i < path.length-1; i++){
+            // out.println("Distance "+path[i]+" + "+path[i+1]+" = "+graph[path[i]][path[i+1]]);
+            dist += graph[path[i]][path[i+1]];
+        }
+        return dist;
+    }
+
+    /**
+     * prints the shortest path to be followed
+     * 
+     * @author Gaurav Agarwal
+     */
+    public void printPath(int path[]){
+        for(int p: path){
+            out.print(p+" --> ");
+        }
+        out.println();
     }
 
     /**
@@ -36,11 +70,69 @@ class ShortestPath{
      * It starts traversing the nodes of the graph 
      * till the destination node is reached.
      * 
-     * 
      * @author Gaurav Agarwal
      * @param args not being used.
      */
     public static void main(String[] args) {
-        
+        int n, s, t;
+        Scanner sc = new Scanner(System.in);
+        out.print("n= ");
+        n = sc.nextInt();
+        out.print("s= ");
+        s = sc.nextInt();
+        out.print("t= ");
+        t = sc.nextInt();
+        out.println();
+        if(s<1 || s>n || t<1 || t> n){
+            out.println("Please enter vertices within the range (1 to "+n+")");
+            System.exit(4);
+        }
+        sc.nextLine();
+        int graph[][] = new int[n+1][n+1];
+        String[] edges;
+        for (int i = 1; i <= n; i++) {
+            edges = sc.nextLine().replaceAll("\\s+","").split(",");
+            for (int j = 1; j <= n; j++) {
+                graph[i][j] = Integer.parseInt(edges[j-1]);
+            }
+        }
+        out.println();
+        // displaying the graph
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= n; j++) {
+        //         out.print(graph[i][j]+"\t");
+        //     }
+        //     out.println();
+        // }
+        ShortestPath sp = new ShortestPath();
+        int noChange[] = {-1,-1,-1};
+
+        int path[] = sp.dijkstra(graph, s, t);
+        out.println("Shortest path from current position 1:");
+        sp.printPath(path);
+        out.println("Distance = "+sp.distance(graph, path, n));
+
+        while(path.length > 2){
+            int changedPath[] = new int[3];
+            changedPath = sp.timeChange(graph);
+            // out.println("Changed path: "+Arrays.equal);
+            if(Arrays.equals(changedPath, noChange)){
+                // for(int r = 0; r < path.length; r++){
+                //     path[r] = path[r+1];
+                // }
+                path = Arrays.copyOfRange(path, 1, path.length);
+                out.println("Shortest path from current position "+path[0]+":");
+                sp.printPath(path);
+            }
+            else{
+                out.println("Traffic change: ("+changedPath[0]+", "+changedPath[1]+") = "+changedPath[2]);
+                graph[changedPath[0]][changedPath[1]] = changedPath[2];
+                out.println("Shortest path from current position "+path[0]+":");
+                path = sp.dijkstra(graph, s, t);
+                sp.printPath(path);
+            }
+            out.println("Distance = "+sp.distance(graph, path, n));
+            out.println();
+        }
     }
 }
